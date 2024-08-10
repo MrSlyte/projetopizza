bd8de8589b1cd3212a5035b39a75f533ca60aafe
Leonardo Castellano <leonardo-r-castellano@outlook.com>
2024-08-10 13:25:15 -0300

    change config and add readme.md

--- projetoPizza.Domain/Repository/PersonRepository.cs
+++ projetoPizza.Domain/Repository/PersonRepository.cs
@@ -4,7 +4,6 @@ using Npgsql;
 using projetoPizza.Domain.Config;
 using projetoPizza.Domain.Interface;
 using projetoPizza.Domain.Model;
-using System.Linq;
 
 namespace projetoPizza.Domain.Repository;
 public record PersonRepository(IOptions<SystemConfiguration> Config) : BaseRepository(Config), IPersonRepository
@@ -12,56 +11,100 @@ public record PersonRepository(IOptions<SystemConfiguration> Config) : BaseRepos
     public async Task<Guid> Insert(CreatePersonModel Model)
     {
         using var connection = new NpgsqlConnection(_connectionString);
-        var sql = @"INSERT INTO People (Name, Nickname, Birthdate, Stack) VALUES (@Name, @Nickname, @Birthdate, @Stack) RETURNING ExternalId";
-
-        var parametersWithStack = new
+        try
         {
-            Name = Model.Nome,
-            NickName = Model.Apelido,
-            Birthdate = Model.Nascimento,
-            Stack = string.Join('|', Model.Stack)
-        };
-        return await connection.ExecuteScalarAsync<Guid>(sql, parametersWithStack);
+            await connection.OpenAsync();
+            var sql = @"INSERT INTO People (Name, Nickname, Birthdate, Stack) VALUES (@Name, @Nickname, @Birthdate, @Stack) RETURNING ExternalId";
+
+            var parametersWithStack = new
+            {
+                Name = Model.Nome,
+                NickName = Model.Apelido,
+                Birthdate = Model.Nascimento,
+                Stack = string.Join('|', Model.Stack)
+            };
+            var externalId = await connection.ExecuteScalarAsync<Guid>(sql, parametersWithStack);
+            return externalId;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+        
     }
 
     public async Task<bool> AlreadyExists(string Nickname)
     {
         using var connection = new NpgsqlConnection(_connectionString);
-        var sql = "SELECT Nickname FROM People WHERE Nickname = @Nickname LIMIT 1";
+        try
+        {
+            await connection.OpenAsync();
+            var sql = "SELECT Nickname FROM People WHERE Nickname = @Nickname LIMIT 1";
 
-        var found = await connection.QuerySingleOrDefaultAsync<string>(sql, new { Nickname });
-        return !string.IsNullOrEmpty(found);
+            var found = await connection.QuerySingleOrDefaultAsync<string>(sql, new { Nickname });
+            return !string.IsNullOrEmpty(found);
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+        
     }
 
     public async Task<GetPersonModel> GetById(Guid Id)
     {
         using var connection = new NpgsqlConnection(_connectionString);
-        var sql = "SELECT ExternalId as Id, Name as Nome, Nickname as Apelido, Birthdate as Nascimento, Stack FROM People WHERE ExternalId = @ExternalId LIMIT 1";
-        var parameters = new DynamicParameters();
-        parameters.Add("ExternalId", Id);
-        var found = await connection.QuerySingleAsync(sql, new { ExternalId = Id });
+        try
+        {
+            await connection.OpenAsync();
+            var sql = "SELECT ExternalId as Id, Name as Nome, Nickname as Apelido, Birthdate as Nascimento, Stack FROM People WHERE ExternalId = @ExternalId LIMIT 1";
+            var parameters = new DynamicParameters();
+            parameters.Add("ExternalId", Id);
+            var found = await connection.QuerySingleAsync(sql, new { ExternalId = Id });
 
-        string stack = Convert.ToString(found.stack);
-        var returnMethod = new GetPersonModel(found.id, found.apelido, found.nome, found.nascimento, stack.Split('|'));
-        return returnMethod;
+            string stack = Convert.ToString(found.stack);
+            var returnMethod = new GetPersonModel(found.id, found.apelido, found.nome, found.nascimento, stack.Split('|'));
+            return returnMethod;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+        
     }
 
     public async Task<IEnumerable<GetPersonModel>> GetByTerm(string Term)
     {
         using var connection = new NpgsqlConnection(_connectionString);
-        Term = $"%{Term}%";
+        try
+        {
+            await connection.OpenAsync();
+            Term = $"%{Term}%";
         var sql = "SELECT ExternalId as Id, Name as Nome, Nickname as Apelido, Birthdate as Nascimento, Stack FROM People WHERE CONCAT(Name, NickName, Stack) LIKE @Term";
         var founds = await connection.QueryAsync(sql, new { Term });
         return (from found in founds
                 let stack = Convert.ToString(found.stack)
                 select new GetPersonModel(found.id, found.apelido, found.nome, found.nascimento, stack.Split('|')));
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
     }
 
     public async Task<int> GetCount()
     {
         using var connection = new NpgsqlConnection(_connectionString);
-        var sql = "SELECT count(Nickname) FROM People";
-        var count = await connection.QuerySingleAsync<int>(sql);
-        return count;
+        try
+        {
+            await connection.OpenAsync();
+            var sql = "SELECT count(Nickname) FROM People";
+            var count = await connection.QuerySingleAsync<int>(sql);
+            return count;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
     }
 }
--- projetoPizza/Program.cs
+++ projetoPizza/Program.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Converters;
 using projetoPizza.Configuration;
 using projetoPizza.Controllers;
 using projetoPizza.Domain.Config;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").AddJsonFile("appsettings.Development.json", true).AddEnvironmentVariables();
@@ -17,7 +18,7 @@ builder.Services.Configure<JsonSerializerSettings>(options =>
             NamingStrategy = new Newtonsoft.Json.Serialization.CamelCaseNamingStrategy()
         });
 });
-
+ServicePointManager.DefaultConnectionLimit = 500;
 var app = builder.Build();
 
 app.Services.GetRequiredService<PeopleController>().Map(app);
